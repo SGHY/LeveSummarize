163211810ed7e9d370303a48f1edd177b3e049ed
sghy <13677909051@163.com>
2018-06-29 13:46:57 +0800

    no message

---
+++ LeveSummarizeTests/LeveSummarizeTests.m
@@ -0,0 +1,39 @@
+//
+//  LeveSummarizeTests.m
+//  LeveSummarizeTests
+//
+//  Created by leve on 2018/6/22.
+//  Copyright © 2018年 leve. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LeveSummarizeTests : XCTestCase
+
+@end
+
+@implementation LeveSummarizeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
